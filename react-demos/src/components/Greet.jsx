4f995f0a59a1582fee670ee60f4aafd356099671
Jawad <Jawadsadiq7373@gmail.com>
2025-04-16 16:25:21 -0700

    My first exprience with Vite

---
+++ react-demos/src/components/Greet.jsx
@@ -0,0 +1,5 @@
+function Greet() {
+  return <div>Hello Greet</div>;
+}
+
+export default Greet;
---
+++ react-demos/src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to my Website!</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
